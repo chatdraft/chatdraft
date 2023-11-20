6f723560e2a25a38f86d07c79a018266f3feadf7
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2023-11-20 14:13:26 -0500

    changed SnapDeck to display as a grid instead of flex

--- src/lib/components/SnapDeck.svelte
+++ src/lib/components/SnapDeck.svelte
@@ -6,20 +6,18 @@
 	export let perRow: 2 | 3 | 4 | 6 = 6;
 </script>
 
-<div class="flex flex-row flex-shrink content-{contentAlign}">
-	{#each Array(perRow) as _, i}
-		<section class="basis-1/{perRow} w-1/{perRow} flex-initial max-h-min">
-			{#each Array(12/perRow) as _, j}
-					{#if cards[j*perRow+i]}
-						<img src={cards[j*perRow+i].displayImageUrl} alt="{cards[j*perRow+i].name}'s card" />
-					{:else}
-						<img
-							class="pt-4 pb-6 px-8"
-							src="https://assets.snap.fan/files/assets/card-frame-placeholder.ef8c62230e9519b4.webp"
-							alt="Placeholder card"
-						/>
-					{/if}
-			{/each}
-		</section>
+<div class="grid-cols-{perRow} content-{contentAlign} grid">
+	{#each Array(12/perRow) as _, i}
+		{#each Array(perRow) as _, j}
+				{#if cards[i*perRow+j]}
+					<img src={cards[i*perRow+j].displayImageUrl} alt="{cards[i*perRow+j].name}'s card" />
+				{:else}
+					<img
+						class="pt-4 pb-6 px-8 flex-none"
+						src="https://assets.snap.fan/files/assets/card-frame-placeholder.ef8c62230e9519b4.webp"
+						alt="Placeholder card"
+					/>
+				{/if}
+		{/each}
 	{/each}
 </div>
