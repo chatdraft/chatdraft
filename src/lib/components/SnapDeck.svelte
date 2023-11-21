bdc3a0c57d8eaab33c76b804660ff3149d422c0c
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2023-11-21 00:14:45 -0500

    flex was working better

--- src/lib/components/SnapDeck.svelte
+++ src/lib/components/SnapDeck.svelte
@@ -5,18 +5,20 @@
 	export let cols: 2 | 3 | 4 | 6 = 6;
 </script>
 
-<div class="grid grid-cols-{cols} content-start">
-	{#each Array(12/cols) as _, i}
-		{#each Array(cols) as _, j}
-				{#if cards[i*cols+j]}
-					<img src={cards[i*cols+j].displayImageUrl} alt="{cards[i*cols+j].name}'s card" />
-				{:else}
-					<img
-						class="pt-4 pb-6 px-8 flex-none"
-						src="https://assets.snap.fan/files/assets/card-frame-placeholder.ef8c62230e9519b4.webp"
-						alt="Placeholder card"
-					/>
-				{/if}
-		{/each}
+<div class="flex flex-row content-start">
+	{#each Array(cols) as _, i}
+		<section class="basis-1/{cols} w-1/{cols} flex-initial max-h-min">
+			{#each Array(12/cols) as _, j}
+					{#if cards[j*cols+i]}
+						<img src={cards[j*cols+i].displayImageUrl} alt="{cards[j*cols+i].name}'s card" />
+					{:else}
+						<img
+							class="pt-4 pb-6 px-8 flex-none"
+							src="https://assets.snap.fan/files/assets/card-frame-placeholder.ef8c62230e9519b4.webp"
+							alt="Placeholder card"
+						/>
+					{/if}
+			{/each}
+		</section>
 	{/each}
 </div>
