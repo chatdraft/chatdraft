8fc6b7d82fa79624b7dc1b26024821a41a5b5e61
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2023-12-09 15:41:02 -0500

    class needed for icon

--- src/routes/(stream)/stream/[player]/+page.svelte
+++ src/routes/(stream)/stream/[player]/+page.svelte
@@ -127,7 +127,7 @@
 											<!-- Vote Icon & Number of Votes-->
 											<div class="flex justify-center absolute bottom-0 right-2">
 												<div class="flex items-center">
-													<iconify-icon icon="mdi:heart" width="32" height="32" flip="horizontal"></iconify-icon>
+													<iconify-icon icon="mdi:heart" width="32" height="32" flip="horizontal" class="h-8 stroke-black stroke-1 fill-white drop-shadow-[-2px_2px_2px_var(--tw-shadow-color)] inline-block transform translate-x-1.5"></iconify-icon>
 													<span class="font-outline">
 														{votes[index]}
 													</span>
