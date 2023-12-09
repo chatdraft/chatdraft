6c60a6ed13edcf4bce10c3557a60871216950af2
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2023-12-09 11:10:19 -0500

    basic about page

--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -4,6 +4,22 @@
 </svelte:head>
 
 <div class="space-y-4 p-4">
-	<h1>about oro's chat draft</h1>
-	<p>oro's chat draft is an proof of concept for letting Twitch Chat draft a Marvel Snap Deck.</p>
+	<h1>About Oro Chat Draft</h1>
+	<p>
+		Oro Chat Draft is an proof of concept for letting Twitch chat draft a Marvel Snap Deck.
+		Twitch chat votes on which card to add to the streamer's deck. A browser source
+		displays the draft status in real time on stream and a bot in chat announces
+		the choices and selections. The streamer doesn't need to interact after
+		starting the draft, and can focus on streaming while chat drafts a deck.
+		After 12 cards have been selected, the streamer can copy and paste the deck
+		code into Marvel Snap.
+	</p>
+	<p>
+		To use the app, you'll need to log in to Twitch and authorize this app 
+		to connect to your account and read and manage channel point redemptions.
+		Then invite the bot to join your Twitch chat. Then, set up the browser sources
+		to display the draft status on stream. The 
+		<a class="anchor" href="start">Get Started</a>
+		page will walk you through each of these steps.
+	</p>
 </div>
