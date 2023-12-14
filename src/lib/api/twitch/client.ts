c3dbe2a5116cd13d68d0963ed9ea0238993d4fc1
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2023-12-14 15:13:55 -0500

    change oauth client id to runtime env variable

--- src/lib/api/twitch/client.ts
+++ src/lib/api/twitch/client.ts
@@ -1,7 +1,6 @@
 import { env } from "$env/dynamic/public";
-import { PUBLIC_TWITCH_OAUTH_CLIENT_ID } from "$env/static/public"
 
 
-export const twitch_login_uri = `https://id.twitch.tv/oauth2/authorize?client_id=${PUBLIC_TWITCH_OAUTH_CLIENT_ID}&redirect_uri=${env.PUBLIC_TWITCH_REDIRECT_URI}&response_type=code&scope=channel%3Aread%3Aredemptions+channel%3Amanage%3Aredemptions`;
+export const twitch_login_uri = `https://id.twitch.tv/oauth2/authorize?client_id=${env.PUBLIC_TWITCH_OAUTH_CLIENT_ID}&redirect_uri=${env.PUBLIC_TWITCH_REDIRECT_URI}&response_type=code&scope=channel%3Aread%3Aredemptions+channel%3Amanage%3Aredemptions`;
 
-export const twitch_bot_uri = `https://id.twitch.tv/oauth2/authorize?client_id=${PUBLIC_TWITCH_OAUTH_CLIENT_ID}&redirect_uri=${env.PUBLIC_TWITCH_REDIRECT_URI}&response_type=code&scope=chat:read+chat:edit`
+export const twitch_bot_uri = `https://id.twitch.tv/oauth2/authorize?client_id=${env.PUBLIC_TWITCH_OAUTH_CLIENT_ID}&redirect_uri=${env.PUBLIC_TWITCH_REDIRECT_URI}&response_type=code&scope=chat:read+chat:edit`
