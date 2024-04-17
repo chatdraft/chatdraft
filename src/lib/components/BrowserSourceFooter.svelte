51a7f36f95df4394c19b95ee67a12f25178df4df
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2024-04-17 18:51:41 -0400

    formatting

--- src/lib/components/BrowserSourceFooter.svelte
+++ src/lib/components/BrowserSourceFooter.svelte
@@ -1,8 +1,8 @@
 <p
 	class="bg-purple-900 text-white rounded-b-lg text-xl flex justify-center font-outline uppercase font-outline-black font-snapa bg-opacity-70"
 >
-	To use Chat Draft, inquire at:&nbsp;<span
-		class="anchor font-serif font-outline-0 normal-case relative bottom-[2px]"
-		>twitch.tv/jjrolk</span
-	>
+	To use Chat Draft, inquire at:&nbsp;
+	<span class="anchor font-serif font-outline-0 normal-case relative bottom-[2px]">
+		twitch.tv/jjrolk
+	</span>
 </p>
