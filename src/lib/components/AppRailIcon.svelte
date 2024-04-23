5520aee14f20dba5f818852b0a6268cd1588d2c6
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2024-04-23 19:34:00 -0400

    added border to current page on nav

--- src/lib/components/AppRailIcon.svelte
+++ src/lib/components/AppRailIcon.svelte
@@ -7,16 +7,18 @@
 		routeId != '/(app)' ? pageRouteId && pageRouteId.includes(routeId) : routeId == pageRouteId;
 </script>
 
-<iconify-icon
-	style:color={pageSelected ? 'rgba(var(--color-primary-500) / 1)' : ''}
-	{icon}
-	width="28"
-	height="28"
-/>
-<p
-	class="mr-2 w-full"
-	style:color={pageSelected ? 'rgba(var(--color-primary-500) / 1)' : ''}
-	class:underline={pageSelected}
->
-	<slot />
-</p>
+<div class="rounded-lg border-primary-500" class:border-2={pageSelected}>
+	<iconify-icon
+		style:color={pageSelected ? 'rgba(var(--color-primary-500) / 1)' : ''}
+		{icon}
+		width="28"
+		height="28"
+	/>
+	<p
+		class="mr-2 w-full"
+		style:color={pageSelected ? 'rgba(var(--color-primary-500) / 1)' : ''}
+		class:underline={pageSelected}
+	>
+		<slot />
+	</p>
+</div>
